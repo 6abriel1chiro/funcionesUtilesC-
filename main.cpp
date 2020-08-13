b1c6b13521bff59cccb01f8d694747994c4be840
Gabriel Ichiro <gabriel582084@gmail.com>
2020-08-12 22:39:42 -0400

    6 en 1

--- main.cpp
+++ main.cpp
@@ -1,242 +1,168 @@
 #include <iostream>
-
+#include <stdio.h>
+#include <conio.h>  // para kbhit()
+#include <windows.h>
+#define COLORNORMAL 7
 using namespace std;
 
-// EJERCICIOS  1 NUMERO MAYOR
-int funcionNumeroMayor(int numeros[],int cantidad){
-int i;
-int mayor;
-for(i=0;i<cantidad;i++){
-if(i==0){
-    mayor=numeros[i];
-}
-else{
-    if(numeros[i]>mayor)
-        mayor=numeros[i];
-}
 
-}
-return mayor;
-}
-//EJERCICIO 2
-void ProcedNumeroMayor(int numeros[],int cantidad,int& mayor){
-int i;
-for(i=0;i<cantidad;i++){
-if(i==0){
-    mayor=numeros[i];
-}
-else{
-    if(numeros[i]>mayor)
-        mayor=numeros[i];
-}
+//EJERCICIO 8
+void dibujarLineaHorizontal(int tam,int posx, int posy,char c){
 
-}
-cout<<"Resultado de PROCEDIMIENTO, el mayor es "<<mayor<<endl;
-}
-// EJERCICIO 3
-void ejer3(int& num1, int& num2)
-{
-    int aux;
-    aux=num1;
-    num1=num2;
-    num2=aux;
-}
-//EJERCICIO 4
-void numerosAbundantes(){
-int sumaDivisores=0;
-	int numero, i, resto;
-	cout << "Este proceso muestra si un numero es abundante" << endl;
-	cout << "Dime un número: ";
-	cin >> numero;
-	cout << "Sus divisores son: ";
-		for (i=1; i<=numero ; i++)
-			{
-			resto = numero%i;
-			if (resto==0)
-				{
-				    if(i!=numero)
-                        {
-				cout << i << " ";
-				sumaDivisores=sumaDivisores+i;
-                        }
-				}
-			}
-	cout << endl;
-	cout<<"suma "<<sumaDivisores<<endl;
-	if(numero<sumaDivisores)
-        cout<<numero<<" es abundante "<<endl;
-	else
-        cout<<numero<<" NO es abundante "<<endl;
-
-
-}
-
-//EJERCICIO 5
-int invertirNumero(int numero){
-
-	int aux ,mult;
-	int numeroR = 0;
-
-	mult = 1;
-	aux = numero;
-
-	while(aux!=0){
-		aux /=10;
-		mult *=10;
-	}
-
-	mult /=10;
-
-	while(numero!=0){
-		aux = numero % 10;
-		numeroR += mult * aux;
-		numero /=10;
-		mult /=10;
-	}
-
-	return numeroR;
-}
-void invertidosIguales(int pInicio,int pFinal){
-    int i,invertido;
-for(i=pInicio;i<pFinal;i++){
-    invertido=invertirNumero(i);
-    if(i==invertido)
-        cout<<i<<endl;
-}
-}
-//EJERCICIO 6
-void leerHora(int &hora,int &minuto,int &segundo){
-   do{
-        cout<<"POR FAVOR INGRESE UNA HORA VALIDA "<<endl;
-        cin>>hora>>minuto>>segundo;
-    } while((hora>24) or (minuto>60) or (segundo>60));
-}
-void mostrarHora(int &hora,int &minuto,int &segundo){
-    cout<<hora<<" :  "<<minuto<<" : "<<segundo<<endl;
-}
-bool esPrimeraHoraEsPosterior(int &h1,int &m1,int &s1,int &h2,int &m2, int &s2){
-
-    if(h1>h2)
-        return true;
-        else if(m1>m2)
-            return true;
-                else if(s1>s2)
-                    return true;
-                    else
-                    return false;
-}
-void HoraMasReciente(){
-    int numero;
-    int hora,minuto,segundo;
-    int hora1,minuto1,segundo1;
-    int horaMasReciente,minutoMasReciente,segundoMasReciente;
-    cout<<"cuantas horas va a comparar? "; cin>>numero;
-    cout<<endl;
-    for(int i=0;i<numero;i++){
-        leerHora(hora,minuto,segundo);
-        if(i==0){
-        hora1=hora;
-        minuto1=minuto;
-        segundo1=segundo;
+for(int i = 0;i<tam;i++){
+        for (int j = 0; j <= i; j++)
+        {
+            cout <<c;
         }
-    if(!esPrimeraHoraEsPosterior(hora1,minuto1,segundo1,hora,minuto,segundo)){
-        horaMasReciente=hora1;
-        minutoMasReciente=minuto1;
-        segundoMasReciente=segundo1;
-    }
-    else{
-        horaMasReciente=hora;
-        minutoMasReciente=minuto;
-        segundoMasReciente=segundo;
-
-    }
-
-        hora1=hora;
-        minuto1=minuto;
-        segundo1=segundo;
-
-    }
-    cout<<"la hora mas reciente es "<<horaMasReciente<<" : "<<minutoMasReciente<<" : "<<segundoMasReciente<<endl;
-}
-//EJERCICIO 7
-void sumarSegundo(int &hora,int &minuto,int &segundo){
-int aSegundos = hora*3600+(minuto*60)+segundo;
-aSegundos += 1;
-
-hora = aSegundos / 3600;
-minuto = ((aSegundos /60)-(hora*60));
-segundo=(aSegundos-(minuto*60)-(hora*3600));
+        cout << endl;
 
 }
+}
+void dibujarLineaVertical(int tam,int posx, int posy,char c)
+{
+for(int i = tam;i>0;i--){
+        for (int j = 0; j < i; j++)
+        {
+            cout <<c;
+        }
+        cout << endl;
 
-//MAIN
+}
+}
+//EJERCICIO 9
+void dibujarLineaHorizontalEjercicioNueve(int tam,int posx, int posy,char c){
+
+for(int i = 0;i<tam;i++){
+        for (int j = 0; j <= i; j++)
+        {
+
+            cout <<c;
+
+               if(c=='z'){
+                c='a';
+            }
+               c++;
+
+
+        }
+        cout << endl;
+
+}
+}
+// EJERCICIO 10
+void dibujarLineaHorizontalEjercicioDiez(int tam,int posx, int posy,int digito){
+
+for(int i = 0;i<tam;i++){
+        for (int j = 0; j <= i; j++)
+        {
+            cout <<digito;
+        }
+        cout << endl;
+
+}
+}
+//EJERCICIO 11
+
+void gotoxy(short x, short y)  //MUEVE EL CURSOR A LA POSICION (X ,Y)
+{
+   COORD pos = { x, y };
+   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+void color(WORD col)
+{
+   SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE),col);
+}
+void dibujaLineaHorizontal(char car, int tamanio,int posx,int posy,int pintura)
+//dibuja una linea con el car de un tamanio del color pintura, a partir de la posx,posy
+{
+    color(pintura);
+    for (int i=0;i<tamanio;i++)
+    {
+        gotoxy(posx+i,posy);
+        cout<<car;
+
+    }
+    color(COLORNORMAL);
+}
+void mostrarFiguraEnUnaPos(char car, int posx, int posy,
+                           int lado, int pintura)
+{
+
+    color(pintura);
+    for (int tam=0;tam<lado;tam++)
+    {
+        dibujaLineaHorizontal(car,tam,posx,posy,pintura);
+        posy++;
+    }
+
+}
+
+void borrarFiguraEnUnaPos(char car,int posx,int posy, int lado,int pintura)
+{
+   for (int tam=0;tam<lado;tam++)
+    {
+        dibujaLineaHorizontal(car,tam,posx,posy,pintura);
+        posy++;
+    }
+
+}
+
+void moverFiguraUnaPosDerecha(char car,int &posx, int posy,int lado,int pintura,int retardo)
+{
+    mostrarFiguraEnUnaPos(car,posx,posy,lado,pintura);
+    Sleep(retardo);
+    borrarFiguraEnUnaPos(car,posx, posy,lado,pintura);
+    posx++;
+    mostrarFiguraEnUnaPos(car,posx,posy,lado,pintura);
+}
+void moverFiguraUnaPosIzquierda(char car,int &posx, int posy,int lado,int pintura,int retardo)
+{
+    mostrarFiguraEnUnaPos(car,posx,posy,lado,pintura);
+    Sleep(retardo);
+    borrarFiguraEnUnaPos(car,posx, posy,lado,pintura);
+    posx--;
+    mostrarFiguraEnUnaPos(car,posx,posy,lado,pintura);
+}
 int main()
 {
 /*
-int cantidad;
-cout<<"cuantos numeros va a comparar?"<<endl;
-cin>>cantidad;
-int numeros[cantidad];
+    int tam;
+    cin>>tam;
 
-for(int i=0;i<cantidad;i++){
-cout<<"ingresa un numero"<<endl;
-cin>>numeros[i];
-}
-//1 FUNCION
-int mayor=funcionNumeroMayor(numeros,cantidad);
-cout<<"RESULTADO DE FUNCION, el mayor es "<<mayor<<endl;
-
-//2 PROCEDIMIENTO
-ProcedNumeroMayor(numeros,cantidad,mayor);
-
-// EJERCICIO 3
-        int a;
-        int b;
-        cout<<"primer valor "; cin>>a;
-        cout<<"segundo valor "; cin>>b;
-         cout<<"\nvalores:  ";
-        cout<< a <<" "<< b<<endl;
-        ejer3(a,b);
-         cout<<"\nvalores nuevos: ";
-        cout<< a <<" "<< b<<endl;
-
-
-
-//EJERCICIO 4   numerosAbundantes();
-
-//EJERCICIO 5
-int a,b;
-cout<<"Mostrar numeros del :"; cin>>a;
-cout<<" al ";  cin>>b;
-invertidosIguales(a,b);
-
-//EJERCICIO 6
-int hora, minuto, segundo;
-int hora2,minuto2,segundo2;
-leerHora(hora,minuto,segundo);
-mostrarHora(hora,minuto,segundo);
-
-    cout<<"ingrese la primera hora ";
-    leerHora(hora,minuto,segundo);
-    cout<<"ingrese la segunda hora ";
-    leerHora(hora2,minuto2,segundo2);
-
-cout<<esPrimeraHoraEsPosterior(hora,minuto,segundo,hora2,minuto2,segundo2)<<endl;
-
-HoraMasReciente();
-*/
-//EJERCICIO 7
-int hora,minuto,segundo;
-int k;
-leerHora(hora,minuto,segundo);
-
-cout<<"cuantos segundos desea aumentar? "; cin>>k;
+// EJERCICIO 8
+dibujarLineaHorizontal(tam,0,0,'*');
 cout<<endl;
-for(int i=0;i<k;i++)
-sumarSegundo(hora,minuto,segundo);
+dibujarLineaVertical(tam,0,0,'*');
 
-cout<<"Nueva hora"<<endl;
-cout<<hora<<" : "<<minuto<<" : "<<segundo<<endl;
+//EJERCICIO 9
+dibujarLineaHorizontalEjercicioNueve(tam,0,0,'a');
+
+//EJERCICIO 10
+
+int digito;
+for(digito;digito<=9;digito++){
+dibujarLineaHorizontalEjercicioDiez(5,0,0,digito);
+dibujarLineaHorizontalEjercicioDiez(7,0,0,digito);
+dibujarLineaHorizontalEjercicioDiez(9,0,0,digito);
+dibujarLineaHorizontalEjercicioDiez(13,0,0,digito);
+
+}
+*/
+//EJERCICIO 11
+int posxDeMiCaracter1=10, posxDeMiCaracter2=10,posxDeMiCaracter3=10;
+
+  // mostrarFiguraEnUnaPos('&',5,10,9,10);
+   //Sleep(3000);
+    //borrarFiguraEnUnaPos(' ',5,10,9,7);
+          for (int i=0;i<20;i++){
+    moverFiguraUnaPosDerecha('&',posxDeMiCaracter1,5,9,10,300);
+    }
+     for (int i=0;i<20;i++){
+    moverFiguraUnaPosIzquierda('&',posxDeMiCaracter1,5,9,10,300);
+     }
+
+     //EJERCICIO 12
 
 
 
